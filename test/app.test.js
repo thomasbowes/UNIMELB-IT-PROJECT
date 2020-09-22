ecef70c61bfb855a21fda1636452eba5769188f4
Keizo Hamaguchi <khamaguchi@student.unimelb.edu.au>
2020-09-22 14:31:49 +1000

    Added comments to test

--- test/app.test.js
+++ test/app.test.js
@@ -1,4 +1,6 @@
 const expect = require('chai').expect;
+
+// to test our endpoints
 const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../app');
@@ -6,13 +8,16 @@ const app = require('../app');
 // Retrieving URI for test mongodb server
 const db = require('../config/keys').testMongoURI;
 
+// connecting to mock database...
 mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const User = require('../models/Users');
 
+// testing our whole application 
 describe('App test', () => {
 	let server;
 
+	// setting things up before testing (inputting test examples)
 	before(function () {
 		const tests = [
 			{
@@ -30,16 +35,23 @@ describe('App test', () => {
 				isAdmin: false
 			}
 		]
+
+		// starting local server
 		server = app.listen(5001);
+
+		// inserting all tests into mock database
 		return User.insertMany(tests);
 	});
 
+	// after finishing tests, delete all records in mock db and closing server
 	after(async function() {
 		await User.deleteMany({});
 		await mongoose.connection.close();
 		await server.close();
 	});
 
+	// tests whether server returns all users in database
+	// P.S this function isn't done yet, needs more tests 
 	describe("Getting all users from database", () => {
 		it("getting all users", function() {
 			return request(app)
