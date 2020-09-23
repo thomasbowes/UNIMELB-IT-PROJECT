71b1bf52457849bd210ab5342b9b25b93c96de8c
Yige Song <soys@student.unimelb.edu.au>
2020-09-23 20:05:16 +1000

    SideDrawer updated
    The sidedrawer also supports multi-paging.
    
    Description in "about" added.

--- client/src/components/Navigation/SideDrawer/SideDrawer.js
+++ client/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import Logo from '../../../assets/Logo/Logo.png'
 import './SideDrawer.css'
 //import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
+import NavBarItems from '../NavBar/NavBarItems/NavBarItems'
 
 class SideDrawer extends Component {
     render () {
@@ -13,13 +14,14 @@ class SideDrawer extends Component {
 
         return (
             <div className={attachedClasses.join(' ')}>
-                {/* <DrawerToggle /> */}
+
                 <img src={Logo} alt="FolioExchangeLogo"/>
-                <a href="#Home">Home</a>
+                {/* <a href="#Home">Home</a>
                 <a href="#BrowseEportfolio">BrowseEportfolio</a>
                 <a href="#MyPage">MyPage</a>
                 <a href="#About">About</a>
-                <a href="#Settings">Settings</a>
+                <a href="#Settings">Settings</a> */}
+                <NavBarItems />
             </div>
 
         );
--- client/src/containers/AboutPage/AboutPage.js
+++ client/src/containers/AboutPage/AboutPage.js
@@ -6,6 +6,13 @@ class AboutPage extends Component{
     render () {
         return (
             <div className='AboutPage'>
+                <h1>About Us</h1>
+                <p className='text'>The folio-exchange website is created and maintained by O(n!), which is a group of five members. Our purpose is to create an 
+                    app for guaduates to upload/show their works, and for employers to search for the portfolio of potential employees.
+                </p>
+
+                <ul></ul>
+
                 <MemberCard name="Thomas"/>
                 <MemberCard name="Song"/>
                 <MemberCard name="Ray"/>
