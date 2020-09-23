b16a29af53c007e271e2a1b6d122b8489fd14405
haoxin-li <haoxinlily@aliyun.com>
2020-09-23 23:27:56 +1000

    Added tests for POST request

--- test/app.test.js
+++ test/app.test.js
@@ -44,19 +44,75 @@ describe('App test', () => {
 	});
 
 	// after finishing tests, delete all records in mock db and closing server
+	/*
 	after(async function() {
 		await User.deleteMany({});
 		await mongoose.connection.close();
 		await server.close();
 	});
+	*/
 
 	// tests whether server returns all users in database
 	// P.S this function isn't done yet, needs more tests 
 	describe("Getting all users from database", () => {
-		it("getting all users", function() {
-			return request(app)
+		it("getting all users", function(done) {
+			request(app)
 				.get('/api/users/alluser')
-				.expect(200);
+				.expect('Content-Type', /json/)
+				.expect(200, done);
 		});
-	});	
+	});
+	
+	// Tests if a POST request (i.e. register) will be made successfully
+	describe("Register a new user in the database", function () {
+		
+		const newUser = {
+			username: 'TinTin',
+			email: 'haoxinli89@gmail.com',
+			password: 'tesing123',
+			confirm: false,
+			isAdmin: false
+		};
+
+		const newUserDupEmail = {
+			username: 'Captain',
+			email: 'haoxinli89@gmail.com',
+			password: 1234,
+			confirm: false,
+			isAdmin: false
+		}
+
+		const newUserNoName = {
+			email: 'haoxinl1@student.unimelb.edu.au',
+			password: 1234
+		}
+		
+		// Question: check that a confirmation email address has been sent?
+		it("Register a new user with unique email", function(done) {
+			request(app)
+				.post('/api/users/register')
+				.send(newUser)
+				.expect('Content-Type', /json/)
+				.expect(201, done);
+		});
+
+		it("Register new user with duplicated email", function(done) {
+			request(app)
+				.post('/api/users/register')
+				.send(newUserDupEmail)
+				.expect('Content-Type', /json/)
+				.expect({
+					status: "Email is already registered"
+				})
+				.expect(200, done);
+		});
+
+		it("Register new user without username", function(done) {
+			request(app)
+				.post('/api/users/register')
+				.send(newUserNoName)
+				.expect('Content-Type', /json/)
+				.expect(400, done)
+		});
+	});
 });
