5eb72232c4f15553063751901d24fece6e092ca7
Yige Song <soys@student.unimelb.edu.au>
2020-09-24 11:55:28 +1000

    Get rid of errors

--- client/src/components/RegisterWindow/RegisterWindow.js
+++ client/src/components/RegisterWindow/RegisterWindow.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import '../LoginWindow/LoginWindow.css';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
 
 class LoginWindow extends Component {
     state = {
