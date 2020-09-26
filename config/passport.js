07c1b0ab1c6fa23fb6a31ea4b8fc7d4d720cf6b1
Keizo Hamaguchi <khamaguchi@student.unimelb.edu.au>
2020-09-26 21:38:48 +1000

    Included fb dev credentials

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const User = require('mongoose').model('User');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 require('dotenv').config();
 
@@ -28,6 +29,15 @@ const strategy = new JwtStrategy(options, (payload, done) => {
 		})
 		.catch((err) => done(err, null));
 });
+
+const fbOptions = {
+	clientID: process.env.FACEBOOK_ID,
+	clientSecret: process.env.FACEBOOK_SECRET
+};
+
+// how the passport middleware is going to authorize a request with facebook credentials	
+const facebookStrategy = new FacebookTokenStrategy(fbOptions, 
+	(accessToken, refreshToken, profile, done) => {});
 
 // which function the passport will provide when invoked
 module.exports = (passport) => {
