2990173f2bde3ec56b83431b55644861f169cb47
Ray Chen <46772434+rayjtchen@users.noreply.github.com>
2020-09-26 22:03:25 +1000

    add login redux and axios setting
    frontend
    - add login redux in auth
    - add axios setting

--- client/src/store/actions/authActions.js
+++ client/src/store/actions/authActions.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-
 import * as actionTypes from './actionTypes';
 
 
@@ -9,20 +8,40 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = () => {
+export const authSuccess = (token, userId) => {
     return {
-        type: actionTypes.AUTH_SUCCESS
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: token,
+        userId: userId
     };
 };
 
-export const authFail = () => {
+export const authFail = (error) => {
     return {
-        type: actionTypes.AUTH_FAIL
+        type: actionTypes.AUTH_FAIL,
+        error: error
     };
 };
 
 export const auth = (email, password) => {
-    return {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password
+        };
+
+        const url = 'http://localhost:5000/api/users/login'
+
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                //dispatch(authSuccess(response.data.idToken, response.data.localId));
+            })
+            .catch(err => {
+                console.log(err);
+                //dispatch(authFail(err));
+            });
     };
 };
 
