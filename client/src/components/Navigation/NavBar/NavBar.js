93d7ff4548cbc8be1955ba201f6435a755783536
Yige Song <soys@student.unimelb.edu.au>
2020-09-26 21:22:38 +1000

    Header/Footer Added, search bar works

--- client/src/components/Navigation/NavBar/NavBar.js
+++ client/src/components/Navigation/NavBar/NavBar.js
@@ -1,21 +1,39 @@
 import React, {Component} from 'react';
 import './NavBar.css';
-import Logo from '../../../assets/Logo/Logo.png';
+import Logo from '../../../assets/Homepage-icons/folioexchange-logo.svg';
 import NavBarItems from '../NavBar/NavBarItems/NavBarItems'
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
+import {Link} from "react-router-dom"
 
 class NavBar extends Component {
     render(){
         return (
-            <div className="NavBar">
-                <div>
-                    <img src={Logo} alt='' />
-                    <DrawerToggle clicked={this.props.sideDrawerClicked}/>
-                </div>
-                
-                <NavBarItems />
+            <header className="NavBar" >
+                <DrawerToggle clicked={this.props.sideDrawerClicked}/>
+                <div className="nav-bar__container">
+                    <div className="main-nav__logo-hamburger">
+                        {/* <DrawerToggle clicked={this.props.sideDrawerClicked}/> */}
+                        <Link to="/home" className="main-header__brand">
+                            <img src={Logo} alt="folio.exchange- logo" style={{float: "left"}}/>
+                        </Link>
+                        <div className="main-header__spacer"></div>
+                    </div>
+                    <nav className="main-nav"> 
+                        <NavBarItems />
+                    </nav>
 
-            </div>
+
+
+                    {/* <div>
+                        <img src={Logo} alt='' />
+                        <DrawerToggle clicked={this.props.sideDrawerClicked}/>
+                    </div>
+                    
+                    <NavBarItems /> */}
+
+                </div>
+            </header>
+    
         );
     }
 }
