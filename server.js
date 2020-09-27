a569c26fa7b15f9b98a66dcc5719ffc7d213d5c0
Yige Song <soys@student.unimelb.edu.au>
2020-09-27 21:33:24 +1000

    Change package.js and server.js file to be ready for automatic run npm build online

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const path = require('path');
+
 // DB Config
 const db = require('./config/keys').mongoURI;
 
@@ -9,6 +11,16 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
     .catch(err=>console.log(err));
 
 const app = require('./app');
+
+// Serve Static assets (massive build file) if we are in production
+if (process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000
 
