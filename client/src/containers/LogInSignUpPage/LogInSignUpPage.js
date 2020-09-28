ecab0d3cca80d9251b47a79cdf9798928c31ee75
Yige Song <soys@student.unimelb.edu.au>
2020-09-28 15:50:24 +1000

    Make the login/SignUp Page a pop up window

--- client/src/containers/LogInSignUpPage/LogInSignUpPage.js
+++ client/src/containers/LogInSignUpPage/LogInSignUpPage.js
@@ -13,47 +13,47 @@ class LogInPage extends Component{
         const passwordInput = "password";
 
         return(
-
             <div className="LoginSignUpPage">
-                <div className="modal__color-bar"></div>
-                <div className="login-signup"> 
-                    <a className="login-signup__close" href="#login-signup__close">
-                        <img src={crossIcon} alt="" />
-                    </a>
-                    <div className="login-signup__form">
-                        <h1>Log In</h1>
-                        <FieldContainer image={emailIcon} placeholder="Email" inputType={textInput}/>
+                <div className="modal">
+                    <div className="modal__color-bar"></div>
+                    <div className="login-signup"> 
+                        <a className="login-signup__close" href="#login-signup__close">
+                            <img src={crossIcon} alt="" />
+                        </a>
+                        <div className="login-signup__form">
+                            <h1>Log In</h1>
+                            <FieldContainer image={emailIcon} placeholder="Email" inputType={textInput}/>
 
-                        <FieldContainer image={keyIcon} placeholder="Password" inputType={passwordInput}/>
-        
-                        <button type="button">Log In</button>
-                        <button type="button">Log In with google</button>
-                        <button type="button">Log Inwith facebook</button>
-                    </div>
-                    <div className="vertical-line"></div>
-                    <div className="login-signup__form">
-                        <h1>Sign Up</h1>
+                            <FieldContainer image={keyIcon} placeholder="Password" inputType={passwordInput}/>
+            
+                            <button type="button">Log In</button>
+                            <button type="button">Log In with google</button>
+                            <button type="button">Log Inwith facebook</button>
+                        </div>
+                        <div className="vertical-line"></div>
+                        <div className="login-signup__form">
+                            <h1>Sign Up</h1>
 
-                        <FieldContainer image={personIcon} placeholder="First Name" inputType={textInput}/>
+                            <FieldContainer image={personIcon} placeholder="First Name" inputType={textInput}/>
 
-                        <FieldContainer image={personIcon} placeholder="Last Name" inputType={textInput}/>
+                            <FieldContainer image={personIcon} placeholder="Last Name" inputType={textInput}/>
 
-                        <FieldContainer image={emailIcon} placeholder="Email" inputType={textInput}/>
+                            <FieldContainer image={emailIcon} placeholder="Email" inputType={textInput}/>
 
-                        <FieldContainer image={keyIcon} placeholder="Password" inputType={passwordInput}/>
-        
-                        <FieldContainer image={keyIcon} placeholder="Confirm Password" inputType={passwordInput}/>
-        
-        
-        
-                        <p>By creating an account you agree to our <a href="#Terms">Terms & Conditions</a> and <a href="#Policy">Privacy Policy</a>.</p>
-                        <button type="button">Sign up</button>
-                        <button type="button">Sign up with google</button>
-                        <button type="button">Sign up with facebook</button>
-                    </div>
-                </div>   
+                            <FieldContainer image={keyIcon} placeholder="Password" inputType={passwordInput}/>
+            
+                            <FieldContainer image={keyIcon} placeholder="Confirm Password" inputType={passwordInput}/>
+            
+            
+            
+                            <p>By creating an account you agree to our <a href="#Terms">Terms & Conditions</a> and <a href="#Policy">Privacy Policy</a>.</p>
+                            <button type="button">Sign up</button>
+                            <button type="button">Sign up with google</button>
+                            <button type="button">Sign up with facebook</button>
+                        </div>
+                    </div>   
+                </div>
             </div>
-
         )
     }
 }
