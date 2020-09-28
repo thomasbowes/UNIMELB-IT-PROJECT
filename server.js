60189ef6f81b305d3305e756da5c0a1c1f30bdf1
haoxin-li <haoxinlily@aliyun.com>
2020-09-28 21:37:56 +1000

    Serve Static Assets in app.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 const path = require('path');
 
 // DB Config
@@ -11,16 +10,6 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
     .catch(err=>console.log(err));
 
 const app = require('./app');
-
-// Serve Static assets (massive build file) if we are in production
-if (process.env.NODE_ENV === 'production'){
-    // Set static folder
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
 
 const port = process.env.PORT || 5000
 
