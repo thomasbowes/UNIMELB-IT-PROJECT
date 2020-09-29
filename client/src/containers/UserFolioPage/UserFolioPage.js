ac637a66c0d6237fef12e2a79d7c1794c1df3597
Yige Song <soys@student.unimelb.edu.au>
2020-09-29 15:16:09 +1000

    Dummy Profile page added

--- client/src/containers/UserFolioPage/UserFolioPage.js
+++ client/src/containers/UserFolioPage/UserFolioPage.js
@@ -1,14 +1,36 @@
 import React, {Component} from 'react';
 import FilesUpload from '../../components/FilesUpload/FilesUpload';
+import eggImage from '../../assets/ProfileImages/egg.jpg'
+import './UserFolioPage.css'
 
-class MyPage extends Component {
+class UserFolioPage extends Component {
     render(){
         return(
-            <div>
+            <div className="UserFolioPage">
+                <div className="User">
+                    <div className="UserPictureHolder">
+                        <img className="UserPicture"src={eggImage} alt='egg' />
+                    </div>
+
+                    <div className="UserInfoHolder">
+                        <div className="UserInfo">
+                            <p><b>Name: </b> Mr. Eggy</p>
+                        </div>
+                        <div className="UserInfo">
+                            <p><b>Birthday: </b> 2023.9.30</p>
+                        </div>
+                        <div className="UserInfo">
+                            <p><b>Education: </b> Eggy Institute of Technology</p>
+                        </div>
+                        <div className="UserInfo">
+                            <p><b>Past experience: </b> Boiled.</p>
+                        </div>
+                    </div>
+                </div>
                 <FilesUpload />
             </div>
         );
     }
 }
 
-export default MyPage;
+export default UserFolioPage;
