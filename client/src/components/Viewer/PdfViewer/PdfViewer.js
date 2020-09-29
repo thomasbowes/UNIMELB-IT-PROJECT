ff4322d7bad26c7fdc06c6e9df27fd45c4b894c9
Yige Song <soys@student.unimelb.edu.au>
2020-09-29 22:29:04 +1000

    The pdf viewer now works

--- client/src/components/Viewer/PdfViewer/PdfViewer.js
+++ client/src/components/Viewer/PdfViewer/PdfViewer.js
@@ -1,25 +1,34 @@
 import React, {useState} from 'react';
 
+import './PdfViewer.css'
+
 import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PdfViewer = (props) => {
     const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(2);
+    const [pageNumber, setPageNumber] = useState(1);
 
     function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
     }
 
+    const lastPage = () => (pageNumber<=1? 1: (pageNumber-1));
+    const nextPage = () => (pageNumber>=numPages? numPages: (pageNumber+1));
+
     return (
-            <div>
-                <Document
+            <div className="PdfViewer">
+                <Document className="Pdf"
                     file={props.file}
                     onLoadSuccess={onDocumentLoadSuccess}
                 >
                     <Page pageNumber={pageNumber} />
                 </Document>
-                <p>Page {pageNumber} of {numPages}</p>
+                <div className="PageChanger">
+                    <button onClick={() => setPageNumber(nextPage())}>Next Page</button>
+                    <p>Page {pageNumber} of {numPages}</p>
+                    <button onClick={() => setPageNumber(lastPage())}>Last Page</button>
+                </div>
             </div>
 
     );
