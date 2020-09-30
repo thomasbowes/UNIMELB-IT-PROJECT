124def37273504feee1b1a82928c17dcd09bb408
Thomas Bowes <41844738+thomasbowes@users.noreply.github.com>
2020-10-01 00:52:11 +1000

    Cleaned first draft of portfolio page

--- client/src/containers/UserFolioPage/UserFolioPage.js
+++ client/src/containers/UserFolioPage/UserFolioPage.js
@@ -60,35 +60,39 @@ class UserFolioPage extends Component {
           const text = "The egg is the organic vessel containing the zygote in which an embryo develops until it can survive on its own, at which point the animal hatches. An egg results from fertilization of an egg cell. Most arthropods, vertebrates (excluding live-bearing mammals), and mollusks lay eggs, although some, such as scorpions, do not. Reptile eggs, bird eggs, and monotreme eggs are laid out of water and are surrounded by a protective shell, either flexible or inflexible. Eggs laid on land or in nests are usually kept within a warm and favorable temperature range while the embryo grows. When the embryo is adequately developed it hatches, i.e., breaks out of the egg's shell. Some embryos have a temporary egg tooth they use to crack, pip, or break the eggshell or covering."
           return (
             <div className="UserFolioPage">
-                <div className="User">
+                <div className="User-info">
                     <div className="UserPictureHolder">
                         <img className="UserPicture"src={eggImage} alt='egg' />
                     </div>
     
                     <div className="UserInfoHolder">
                         <div className="UserInfo">
-                            <p><b>Name: </b> Mr. Eggy</p>
+                            <h1>Mr. Eggy Egglington</h1>
                         </div>
                         <div className="UserInfo">
-                            <p><b>Birthday: </b> 2023.9.30</p>
+                            <h2>An eggcellent student at Eggy Institute of Technology</h2> 
                         </div>
-                        <div className="UserInfo">
-                            <p><b>Education: </b> Eggy Institute of Technology</p>
-                        </div>
-                        <div className="UserInfo">
-                            <p><b>Past experience: </b> Boiled.</p>
+                        <div className="Objective">
+                            <p>A dedicated expert in the field of eggnomics, pushing egg-legislation to be beneficial for your average egg. With my extensive egg-u-cation i bring a dynamic off eggspertise to wherever i work. {text}</p> 
                         </div>
                     </div>
                 </div>
                 <ProfileBlockWithImage image={google2} text={text} title="Founded Eooggle" />
                 <ProfileBlockNoImage text={text} title="Founded Eooggle" />
                 <ProfileBlockTwoProject texts={[text, text]} titles={["Founded Eooggle", "Founded Eggipedia"]} />
+                <div className="ImageGallery">   
+                    <ImageGallery items={images} 
+                        showThumbnails={false}
+                        autoPlay={true}
+                    />
+                </div> 
                 <EducationHistory
                         schools={["Eggy Junior High", "University of Eggplication", "Institute of Making Benedict Egg"]} 
                         descriptions={[text, text, text]} 
                         images={[google1, google1, google2]}
                         durations={["2022-2024", "2024-2027", "2027-2???"]} />
                 
+                <h1>Everything below this is for testing purposes. this is first draft of the profile</h1>
                 <div className="test">
                     <PdfPreview file={eggPdf} clicked={this.showPdfToggle}/>
                 </div>
@@ -96,11 +100,7 @@ class UserFolioPage extends Component {
                 {this.state.showPdf? <BackDrop clicked={this.showPdfToggle} show={this.state.showPdf}/>:null}
                 {this.state.showPdf? <PdfViewer file={eggPdf} />:null}
                 
-                <h1>My image folio</h1>
-                <ImageGallery items={images} 
-                    showThumbnails={false}
-                    autoPlay={true}
-                />
+                
                 
                 <FilesUpload />
             </div>
