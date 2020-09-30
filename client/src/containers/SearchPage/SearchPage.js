3e923508905b817d5b044add523b126f0633dcb6
Yige Song <soys@student.unimelb.edu.au>
2020-09-30 20:22:54 +1000

    Dynamic import tried

--- client/src/containers/SearchPage/SearchPage.js
+++ client/src/containers/SearchPage/SearchPage.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/Auxiliary/Auxiliary'
-import './SearchPage.css'
 import './SearchItem/SearchItem'
 import SearchItem from './SearchItem/SearchItem';
 
 class SearchPage extends Component {
+
+    cssName = () => {
+        if (true) {
+            return import('./SearchPage.css')
+        }
+        else{
+            return import('./SearchPage.css')
+        }
+      };
+
     render(){
+        this.cssName();
         return (
             <Aux>
                 <div className="search__info">Showing 10 of 1009 results:</div>
