58fdcf98999e732133a9cde44c439590b50737a4
Ray Chen <46772434+rayjtchen@users.noreply.github.com>
2020-10-06 16:56:43 +1100

    add upload to Clodinary middleware
    add upload to Clodinary middleware

--- routes/api/portfolio.js
+++ routes/api/portfolio.js
@@ -2,13 +2,20 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
+//Middleware
+const uploadMiddleware = require('../../middleware/upload');
+
 // Item Model and Controller
 const portfolioController = require('../../controllers/portfolioController');
 
 
+
 router.use('/upload', passport.authenticate('jwt', { session: false } ));
+router.use('/upload', uploadMiddleware.uploadFileToCloudinary)
 router.route('/upload')
-    .post(portfolioController.uploadFileToCloudinary);
+    .post((req, res) => {
+        console.log(req.upload_file)
+    });
 
 module.exports = router;
 
