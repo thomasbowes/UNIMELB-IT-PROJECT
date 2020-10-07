0769dc1c16804dd0c007aa077bf5360aee16f865
GuchiAtWork <52662258+GuchiAtWork@users.noreply.github.com>
2020-10-07 22:50:15 +1100

    Update portfolio.js

--- routes/api/portfolio.js
+++ routes/api/portfolio.js
@@ -5,10 +5,16 @@ const passport = require('passport');
 //Middleware
 const uploadMiddleware = require('../../middleware/upload');
 
+// Add auth middleware
+// const authMiddleware = require('../../middleware/authorization');
+
 // Item Model and Controller
 const portfolioController = require('../../controllers/portfolioController');
 
+// Instead of passport.authenticate, replace with
+// router.use('/upload', authMiddleware.authenticateJWT);
 router.use('/upload', passport.authenticate('jwt', { session: false } ));
+
 router.use('/upload', uploadMiddleware.uploadFileVerify);
 router.use('/upload', uploadMiddleware.uploadFileToCloudinary);
 router.route('/upload')
