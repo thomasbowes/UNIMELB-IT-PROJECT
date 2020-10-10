75c0ad07bde399df62ab79a70d229351c1c44e03
Yige Song <soys@student.unimelb.edu.au>
2020-10-10 23:24:10 +1100

    Created the userPage.js and css

---
+++ client/src/containers/UserFolioPage/ProjectPage/ProjectPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ProjectPage extends Component {
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ProjectPage;
