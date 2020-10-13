e5935d342b21262a2f39eb8afeb27df117f62287
Yige Song <soys@student.unimelb.edu.au>
2020-10-13 13:41:58 +1100

    Save some static words as variables, create UserProfile Component

---
+++ client/src/components/ProfilePageFileTemplate/UserProfile/UserProfile.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import './UserProfile.css';
+
+import eggImage from '../../../assets/ProfilePageDocuments/egg.jpg'
+
+class UserProfile extends Component{
+
+    render(){
+        let name = "Mr. Eggy Egglington";
+        let highLevelDes = "An eggcellent student at Eggy Institute of Technology";
+        let description = "A dedicated eggspert in the field of eggnomics, pushing egg-legislation to be beneficial for your average egg. With my extensive egg-u-cation i bring a dynamic off eggspertise to wherever i work."
+        
+        return (
+            <div className="User-info">
+                <div className="UserPictureHolder">
+                    <img className="UserPicture"src={eggImage} alt='egg' />
+                </div>
+
+                <div className="UserInfoHolder">
+                    <div className="UserInfo">
+                        <h1>{name}</h1>
+                    </div>
+                    <div className="UserInfo">
+                        <h2>{highLevelDes}</h2> 
+                    </div>
+                    <div className="Objective">
+                        <p>{description}</p> 
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+export default UserProfile;
--- client/src/containers/UserFolioPage/UserFolioPage.js
+++ client/src/containers/UserFolioPage/UserFolioPage.js
@@ -8,9 +8,9 @@ import EducationHistory from '../../components/ProfilePageFileTemplate/Education
 
 import google1 from '../../assets/ProfilePageDocuments/google.png';
 import google2 from '../../assets/ProfilePageDocuments/google2.jpg';
-import eggImage from '../../assets/ProfilePageDocuments/egg.jpg'
 
 import "react-image-gallery/styles/css/image-gallery.css";
+import UserProfile from '../../components/ProfilePageFileTemplate/UserProfile/UserProfile'
 
 
 class UserFolioPage extends Component {
@@ -20,23 +20,8 @@ class UserFolioPage extends Component {
           const text = "The egg is the organic vessel containing the zygote in which an embryo develops until it can survive on its own, at which point the animal hatches. An egg results from fertilization of an egg cell. Most arthropods, vertebrates (excluding live-bearing mammals), and mollusks lay eggs, although some, such as scorpions, do not. Reptile eggs, bird eggs, and monotreme eggs are laid out of water and are surrounded by a protective shell, either flexible or inflexible. Eggs laid on land or in nests are usually kept within a warm and favorable temperature range while the embryo grows. When the embryo is adequately developed it hatches, i.e., breaks out of the egg's shell. Some embryos have a temporary egg tooth they use to crack, pip, or break the eggshell or covering."
           return (
             <div className="UserFolioPage">
-                <div className="User-info">
-                    <div className="UserPictureHolder">
-                        <img className="UserPicture"src={eggImage} alt='egg' />
-                    </div>
-    
-                    <div className="UserInfoHolder">
-                        <div className="UserInfo">
-                            <h1>Mr. Eggy Egglington</h1>
-                        </div>
-                        <div className="UserInfo">
-                            <h2>An eggcellent student at Eggy Institute of Technology</h2> 
-                        </div>
-                        <div className="Objective">
-                            <p>A dedicated expert in the field of eggnomics, pushing egg-legislation to be beneficial for your average egg. With my extensive egg-u-cation i bring a dynamic off eggspertise to wherever i work. {text}</p> 
-                        </div>
-                    </div>
-                </div>
+
+                <UserProfile />
 
                 <h2>My eggucation history</h2>
 
