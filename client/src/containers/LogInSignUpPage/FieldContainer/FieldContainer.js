a2320912de16c1e51a6963eadf2deaee941a100e
Thomas Bowes <41844738+thomasbowes@users.noreply.github.com>
2020-10-15 02:58:00 +1100

    added login signup error messages, colours, password length restrictions and cleaned up a problem on mobiles

--- client/src/containers/LogInSignUpPage/FieldContainer/FieldContainer.js
+++ client/src/containers/LogInSignUpPage/FieldContainer/FieldContainer.js
@@ -1,12 +1,32 @@
 import React, {Component} from 'react';
-import './FieldContainer.css';
+
+import classes from './FieldContainer.module.css';
 
 class FieldContainer extends Component {
+    errorInformHandler = () => {
+        if (this.props.isError != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
     render(){
+
+        let iconClass = [classes["field-container__icon"]];
+        let inputClass = [classes["field-container__input-field"]]
+
+        if (this.errorInformHandler()) {
+            iconClass.push(classes.Red);
+            inputClass.push(classes.Red);
+        }
+
+        
         return (
-            <div className="field-container">
-                <img className="field-container__icon" src={this.props.image} alt=""></img>
-                <input className="field-container__input-field" 
+            <div className={classes["field-container"]}>
+                <img className={iconClass.join(" ")} src={this.props.image} alt=""></img>
+                <input className={inputClass.join(" ")} 
                     type={this.props.inputType} 
                     placeholder={this.props.placeholder} 
                     name={this.props.placeholder}
