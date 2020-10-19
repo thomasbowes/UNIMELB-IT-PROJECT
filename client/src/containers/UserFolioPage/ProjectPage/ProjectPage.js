c0fef455cc4dc2a4d278f837b3fd4d28972b8d11
Yige Song <soys@student.unimelb.edu.au>
2020-10-19 11:06:15 +1100

    The Project description is now editable locally

--- client/src/containers/UserFolioPage/ProjectPage/ProjectPage.js
+++ client/src/containers/UserFolioPage/ProjectPage/ProjectPage.js
@@ -8,20 +8,40 @@ import PdfPreview from '../../../components/Viewer/PdfPreview/PdfPreview';
 import BackDrop from '../../../components/UI/BackDrop/BackDrop'
 import FilesUpload from '../../../components/FilesUpload/FilesUpload';
 import ImgUpload from '../../../components/FilesUpload/ImgUpload';
+import Aux from '../../../hoc/Auxiliary/Auxiliary'
 
 import eggImg1 from '../../../assets/ProfilePageDocuments/egg1.jpg'
 import eggImg2 from '../../../assets/ProfilePageDocuments/egg2.jpg'
 import eggImg3 from '../../../assets/ProfilePageDocuments/egg3.jpg'
 import eggImg4 from '../../../assets/ProfilePageDocuments/egg4.jpg'
 import eggImg5 from '../../../assets/ProfilePageDocuments/egg5.jpg'
+import EditForm from '../../../components/ProfilePageFileTemplate/EditForm/EditForm';
+
+const defaultTitle = "Default Title"
+const defaultDes = "Default Description: The egg is the organic vessel containing the zygote in which an embryo develops until it can survive on its own, at which point the animal hatches. An egg results from fertilization of an egg cell. Most arthropods, vertebrates (excluding live-bearing mammals), and mollusks lay eggs, although some, such as scorpions, do not. Reptile eggs, bird eggs, and monotreme eggs are laid out of water and are surrounded by a protective shell, either flexible or inflexible. Eggs laid on land or in nests are usually kept within a warm and favorable temperature range while the embryo grows. When the embryo is adequately developed it hatches, i.e., breaks out of the egg's shell. Some embryos have a temporary egg tooth they use to crack, pip, or break the eggshell or covering.The egg is the organic vessel containing the zygote in which an embryo develops until it can survive on its own, at which point the animal hatches. An egg results from fertilization of an egg cell. Most arthropods, vertebrates (excluding live-bearing mammals), and mollusks lay eggs, although some, such as scorpions, do not. Reptile eggs, bird eggs, and monotreme eggs are laid out of water and are surrounded by a protective shell, either flexible or inflexible. Eggs laid on land or in nests are usually kept within a warm and favorable temperature range while the embryo grows. When the embryo is adequately developed it hatches, i.e., breaks out of the egg's shell. Some embryos have a temporary egg tooth they use to crack, pip, or break the eggshell or covering."
 
 
 class ProjectPage extends Component {
-    state = {showPdf: false}
+    state = {
+        showPdf: false,
+
+        titleDesEditable: false,
+        filesEditable: false,
+
+        title: defaultTitle,
+        description: defaultDes}
 
     showPdfToggle = () => {
         const newShowPdf = !this.state.showPdf;
         this.setState({showPdf: newShowPdf})
+    }
+
+    changeTitleDes = (inputs) => {
+        this.setState({title: inputs[0], description: inputs[1]})
+    }
+
+    changeTitleDesEditable = () => {
+        this.setState({titleDesEditable: !this.state.titleDesEditable})
     }
 
     render(){
@@ -50,7 +70,17 @@ class ProjectPage extends Component {
         return (
             <div className="ProjectPage">
 
-                <h1>Items in my projeggct</h1>
+                {this.state.titleDesEditable? 
+                    <EditForm values={[this.state.title, this.state.description]} 
+                        changeEditable = {this.changeTitleDesEditable} 
+                        changeValues={this.changeTitleDes}
+                        fields={["Project Title", "Project Description"]}/>
+                :   <Aux>
+                        <h1>{this.state.title}</h1>
+                        <p>{this.state.description}</p>
+                        <button onClick={this.changeTitleDesEditable}>Change project title/description</button>
+                    </Aux>
+                    }
 
                 <div className="ImageGallery">   
                     <ImageGallery items={images} 
