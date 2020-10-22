04f747f69568e849f8808b72f647d925e0fddcbc
Keizo Hamaguchi <khamaguchi@student.unimelb.edu.au>
2020-10-22 12:36:01 +1100

    Added new attributes to models

--- models/ProfileBlocks.js
+++ models/ProfileBlocks.js
@@ -9,7 +9,11 @@ const ProfileBlockSchema = new mongoose.Schema({
     // replace default value with cloudinary url
     urlProfile: {type: String, default: "https://res.cloudinary.com/dg3osx8ob/image/upload/v1602739213/defaultProfile_puyltp.png"},
     public_id: {type: String , required: false},
-    date: {type: Date, default: Date.now, immutable: true}
+    date: {type: Date, default: Date.now, immutable: true},
+    // latest attributes
+    website: {type: String, required: false},
+    phone: {type: String, required: false},
+    location: {type: String, required: false}
 }, {collection: 'profileblocks'});
 
 const ProfileBlock = mongoose.model('ProfileBlock', ProfileBlockSchema);
